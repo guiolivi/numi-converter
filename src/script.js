175e27da13fcdc9314fce133f66469732e5e1dd1
guiolivi <guilhermeoli.dev@gmail.com>
2026-01-29 23:59:32 -0300

    feat: finish making function

--- src/script.js
+++ src/script.js
@@ -11,17 +11,48 @@ const swapButton = document.querySelector("#swap");
 
 document.addEventListener("click", (e) => {
     const currencyE = e.target.closest(".currency"); if (!currencyE) return;
-    let wrapperE = currencyE.closest(".currency_wrapper"); if (!wrapperE) return;
+    const wrapperE = currencyE.closest(".currency_wrapper"); if (!wrapperE) return;
     const selectionE = wrapperE.querySelector(".currency_selection"); if (!selectionE) return;
     selectionE.classList.toggle("visible");
 })
 
 document.addEventListener("click", (e) => {
     const addE = e.target.closest("#add"); if (!addE) return;
-    let wrapperE = addE.closest(".add-selection_wrapper"); if (!wrapperE) return;
+    const wrapperE = addE.closest(".add-selection_wrapper"); if (!wrapperE) return;
     const selectionE = wrapperE.querySelector(".currency_selection"); if (!selectionE) return;
     selectionE.classList.toggle("visible");
 })
+
+const currencyInfo = async () => {
+    try {
+        const res = await axios.get(
+            "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2026.1.29/v1/currencies.json"
+        );
+        return res.data; // object: { usd: "United States Dollar", ... }
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+function requestInfo() {
+    currencyInfo().then(currencies => {
+        if (!currencies) return;
+
+        Object.entries(currencies).forEach(([abbr, fullName]) => {
+            const template = document.createElement("template");
+
+            template.innerHTML = `
+                <button class="selection_option">
+                    <span class="option_abbreviation">${abbr.toUpperCase()}</span>
+                    <span class="option_text">${fullName}</span>
+                </button>
+            `;
+
+        });
+    });
+}
+
+
 
 const inputBox = document.querySelector("#input");
 const equalsTo = document.querySelector("#equals");
